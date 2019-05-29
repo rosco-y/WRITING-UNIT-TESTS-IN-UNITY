ab2859b7394c9e8db68b87f827c14e445d3b2d7a
Ross Ylitalo <ross.ylitalo@gmail.com>
2019-05-29 16:06:13 -0400

    Added basic project, and a nunit test project (via Unity's Test Runner Window.) along with a Test class, a simple class and a few custom tests.

---
+++ WRITING-UNIT-TESTS-IN-UNITY/Assets/Tests/NewTestScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+
+        myClass classy;
+        [SetUp]
+        public void SetupMyClass()
+        {
+            classy = new myClass();
+        }
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        [Test]
+        public void BasicTest()
+        {
+            bool isActive = true;
+
+            Assert.AreEqual(true, isActive, $"{true} != {isActive}.");
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+
+        [Test]
+        public void CheckMyClass()
+        {
+            classy.CheckThis();
+
+            Assert.AreEqual(classy.CheckThis(), true, $"classy.CheckThis() = {classy.CheckThis()}, NOT {false}!");
+        }
+    }
+}
